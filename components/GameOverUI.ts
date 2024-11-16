62cc3610d9574610d39c303d75957a4218497250
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2024-11-16 23:29:13 +0300

    refactor: made a GameOver class

---
+++ components/GameOverUI.ts
@@ -0,0 +1,33 @@
+import Phaser from 'phaser';
+
+export default class GameOverUI {
+  private container!: Phaser.GameObjects.Container;
+
+  constructor(private scene: Phaser.Scene, private onRestart: () => void) {
+    const background = scene.add.rectangle(600, 340, 1200, 750, 0x000000, 0.7);
+    const gameOverText = scene.add.text(600, 250, 'GAME OVER', {
+      font: '48px Arial',
+      fill: '#ffffff',
+    }).setOrigin(0.5);
+
+    const restartButton = scene.add.text(600, 400, 'Try again', {
+      font: '32px Arial',
+      fill: '#ff0000',
+      backgroundColor: '#ffffff',
+    })
+      .setOrigin(0.5)
+      .setInteractive({ useHandCursor: true })
+      .on('pointerdown', this.onRestart);
+
+    this.container = scene.add.container(0, 0, [background, gameOverText, restartButton]);
+    this.container.setVisible(false);
+  }
+
+  show() {
+    this.container.setVisible(true);
+  }
+
+  hide() {
+    this.container.setVisible(false);
+  }
+}
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import Player from '../../components/Player'
 import Enemy from '../../components/Enemy'
+import GameOverUI from '../../components/GameOverUI'
 import background from '../assets/backgroundGame.png';
 import playerStand from '../assets/pixel/Tiles/Characters/tile_0000.png';
 import playerMove from '../assets/pixel/Tiles/Characters/tile_0001.png';
@@ -21,7 +22,7 @@ export default class GameScene extends Phaser.Scene {
     jump: Phaser.Input.Keyboard.Key;
   };
   private enemies!: Enemy;
-  private gameOverContainer!: Phaser.GameObjects.Container;
+  private gameOverUI!: GameOverUI;
 
   constructor() {
     super('GameScene');
@@ -39,6 +40,13 @@ export default class GameScene extends Phaser.Scene {
     this.add.image(600, 340, 'background');
 
     this.player = new Player(this, 50, 550);
+
+
+    this.enemies = new Enemy(this)
+
+    this.gameOverUI = new GameOverUI(this, () => {
+      this.scene.restart();
+    });
 
     this.keys = this.input.keyboard?.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
@@ -62,21 +70,15 @@ export default class GameScene extends Phaser.Scene {
       strokeThickness: 1
     })
 
-    //Враги
-    this.enemies = new Enemy(this)
-
     this.enemies.startSpawning(2000)
 
     this.physics.add.collider(this.player.getSprite(), this.enemies.getGroup(), this.handlePlayerHit, undefined, this)
 
     this.physics.add.collider(this.player.getBullets(), this.enemies.getGroup(), this.handleBulletHitEnemy, undefined, this);
-
-    this.createGameOverScreen();
   }
 
   update() {
     this.player.handleMovement(this.keys);
-
   }
 
   // урон по игроку
@@ -107,28 +109,8 @@ export default class GameScene extends Phaser.Scene {
     this.enemies.stopSwawning();
     this.enemies.clearEnemies();
     this.player.getSprite().setVisible(false);
-    this.gameOverContainer.setVisible(true);
-  }
-  // создание окошка GameOver
-  private createGameOverScreen() {
-    const background = this.add.rectangle(600, 340, 1200, 750, 0x000000, 0.7);
-
-    const gameOverText = this.add.text(600, 250, 'GAME OVER', {
-      font: '48px Arial',
-      fill: '#ffffff'
-    }).setOrigin(0.5);
-
-    const restartButton = this.add.text(600, 400, 'Try again', {
-      font: '32px Arial',
-      fill: '#ff0000',
-      backgroundColor: '#ffffff',
-    })
-      .setOrigin(0.5)
-      .setInteractive({ useHandCursor: true })
-      .on('pointerdown', () => this.scene.restart())
-
-    this.gameOverContainer = this.add.container(0, 0, [background, gameOverText, restartButton])
-    this.gameOverContainer.setVisible(false);
+    this.physics.pause();
+    this.gameOverUI.show();
   }
 
 }
